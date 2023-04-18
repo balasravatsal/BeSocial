a30e56fe128c2dd0c6ac53e134137eb908e85c5a
Vatsal <vatsalbalasra24@gmail.com>
2023-04-18 17:02:44 +0530

    commented old schema code

--- models/feedbackSchema.js
+++ models/feedbackSchema.js
@@ -1,21 +1,26 @@
-const con = require('./ngo')
+// ******* Dont run **********
 
-// to  create feedback schema of NGOs
-// used during seeding or initially
 
-const makeFeedbackSchema = async() => {
-    const createFeedbackSchema = `
-        CREATE TABLE feedbackSchema (
-            feedbackID char(36) primary key,
-            body varchar(500),
-            rating int,
-            ngoID char(36),
-            FOREIGN KEY (ngoID) REFERENCES ngoSchema(id)
-        )`
-    await con.query(createFeedbackSchema, (error, result) => {
-        if (error) throw error
-        else console.log(result);
-    })
-}
 
-makeFeedbackSchema()
+
+// const con = require('./ngo')
+//
+// // to  create feedback schema of NGOs
+// // used during seeding or initially
+//
+// const makeFeedbackSchema = async() => {
+//     const createFeedbackSchema = `
+//         CREATE TABLE feedbackSchema (
+//             feedbackID char(36) primary key,
+//             body varchar(500),
+//             rating int,
+//             ngoID char(36),
+//             FOREIGN KEY (ngoID) REFERENCES ngoSchema(id)
+//         )`
+//     await con.query(createFeedbackSchema, (error, result) => {
+//         if (error) throw error
+//         else console.log(result);
+//     })
+// }
+//
+// makeFeedbackSchema()
--- models/needySchema.js
+++ models/needySchema.js
@@ -1,14 +1,17 @@
-const con = require('./ngo')
+// ******* Dont run **********
 
-// to  create feedback schema of NGOs
-// used during seeding or initially
 
-const makeNeedySchema = async() => {
-    const createUserSchema = `CREATE TABLE needySchema ( needyID char(36) not null primary key, needyUsername VARCHAR(50) NOT NULL, Password VARCHAR(255) NOT NULL);`
-    await con.query(createUserSchema, (error, result) => {
-        if (error) throw error
-        else console.log(result);
-    })
-}
-
-makeNeedySchema()
+// const con = require('./ngo')
+//
+// // to  create feedback schema of NGOs
+// // used during seeding or initially
+//
+// const makeNeedySchema = async() => {
+//     const createUserSchema = `CREATE TABLE needySchema ( needyID char(36) not null primary key, needyUsername VARCHAR(50) NOT NULL, Password VARCHAR(255) NOT NULL);`
+//     await con.query(createUserSchema, (error, result) => {
+//         if (error) throw error
+//         else console.log(result);
+//     })
+// }
+//
+// makeNeedySchema()
